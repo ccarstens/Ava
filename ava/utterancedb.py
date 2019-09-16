9bbe0c1c05c482d3bff61c3525f31f08666b8674
Cornelius Carstens <cornelius.carstens@me.com>
2019-09-16 13:26:08 -0700

    utterace db init

---
+++ ava/utterancedb.py
@@ -0,0 +1,4 @@
+
+class UtteranceDB():
+    def __init__(self):
+        pass
--- env.py
+++ env.py
@@ -1,4 +1,6 @@
 ASL_AVA = "/Users/cornelius/Development/ava/asl/ava.asl"
 ASL_USER = "/Users/cornelius/Development/ava/asl/user.asl"
 
+UTTERANCE_DB_FILE = "/Users/cornelius/Development/ava/storage/utterances.json"
+
 WIT_API_KEY = "IDPSVO27WMIWQ3QW6YNOWZDRXBXB22XT"
