a683feef36d39c0e97d25d78d3cffd2ac3082a93
Cornelius Carstens <cornelius.carstens@me.com>
2019-09-18 09:55:15 -0700

    rm env

--- env.py
+++
@@ -1,6 +0,0 @@
-ASL_AVA = "/Users/cornelius/Development/ava/asl/ava/main.asl"
-ASL_USER = "/Users/cornelius/Development/ava/asl/usercontroller/main.asl"
-
-UTTERANCE_DB_FILE = "/Users/cornelius/Development/ava/storage/utterances.json"
-
-WIT_API_KEY = "RX5J7NZOP5V3S3AM423PY6MXA4YKKMKR"
