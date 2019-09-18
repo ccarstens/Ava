5b50847161132c32b5db329b7e9b409687245e75
Cornelius Carstens <cornelius.carstens@me.com>
2019-09-18 09:53:16 -0700

    sample env

---
+++ env-sample.py
@@ -0,0 +1,6 @@
+ASL_AVA = "/Global/Path/To/ASL/File.asl"
+ASL_USER = "/Global/Path/To/ASL/File.asl"
+
+UTTERANCE_DB_FILE = "/Global/Path/To/Utterances/File.json"
+
+WIT_API_KEY = "YOUR WIT SERVER ACCESS TOKEN"
--- env.py
+++ env.py
@@ -3,4 +3,4 @@ ASL_USER = "/Users/cornelius/Development/ava/asl/usercontroller/main.asl"
 
 UTTERANCE_DB_FILE = "/Users/cornelius/Development/ava/storage/utterances.json"
 
-WIT_API_KEY = "IDPSVO27WMIWQ3QW6YNOWZDRXBXB22XT"
+WIT_API_KEY = "RX5J7NZOP5V3S3AM423PY6MXA4YKKMKR"
